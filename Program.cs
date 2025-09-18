668407fcff577a1dca624fb044a3add20526084d
muhammadumer811 <muhammadumer8110@gmail.com>
2025-09-18 06:21:13 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ProductwithBL.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 👉 Add connection string
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
